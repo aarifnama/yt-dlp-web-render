f33ee28f1cf420122757d19b3d31921ec6539a91
aarifnama <aarifnama@gmail.com>
2025-11-05 02:40:42 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, send_file
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    ydl_opts = {
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+        'format': 'best'
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+            return send_file(filename, as_attachment=True)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
